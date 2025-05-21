0f2d005f8a70f95ffc6b3163afc8685c3039a813
Vitória Silva da Costa <vitoria1.silva.costa@gmail.com>
2025-05-21 16:42:21 -0300

    chore: integrar Allure Reports ao projeto Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.js",
     baseUrl: "https://front.serverest.dev",
     supportFile: "cypress/support/e2e.js",
+
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+
+    env: {
+      allureResultsPath: 'allure-results' 
+    }
   }
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,26 +6,30 @@ describe('Testes E2E - Login', () => {
   let mensagens;
 
   before(() => {
-    // Carrega os dados dos fixtures antes de rodar os testes
     cy.fixture('usuarios').then((dados) => { usuarios = dados.usuarios; });
     cy.fixture('mensagens').then((dados) => { mensagens = dados.erros; });
   });
 
   beforeEach(() => {
-    // Garante que a página de login será visitada antes de cada teste para isolamento
     LoginPage.acessarPaginaDeLogin();
   });
 
   it('Deve realizar login com sucesso usando dados válidos', () => {
+    cy.allure().startStep('Início do teste: login válido');
+
     LoginPage.preencherEmail(usuarios.validos.email);
     LoginPage.preencherSenha(usuarios.validos.senha);
     LoginPage.clicarEmEntrar();
 
     cy.url().should('include', '/home');
     cy.contains('Logout').should('be.visible');
+
+    cy.allure().endStep('passed');
   });
 
   it('Deve exibir erro ao tentar realizar login com email e senha inválidos', () => {
+    cy.allure().startStep('Início do teste: login inválido com email e senha falsos');
+
     const emailFake = faker.internet.email();
     const senhaFake = faker.internet.password(8);
 
@@ -33,19 +37,27 @@ describe('Testes E2E - Login', () => {
     LoginPage.preencherSenha(senhaFake);
     LoginPage.clicarEmEntrar();
 
-    cy.url().should('include', '/login'); // Garante que permaneceu na página de login
+    cy.url().should('include', '/login');
     LoginPage.validarMensagemErro(mensagens.emailOuSenhaInvalidos);
+
+    cy.allure().endStep('passed');
   });
 
   it('Deve exibir erro ao tentar realizar login sem informar email e senha', () => {
+    cy.allure().startStep('Início do teste: login sem email e senha');
+
     LoginPage.clicarEmEntrar();
 
     cy.url().should('include', '/login');
     LoginPage.validarMensagemErro(mensagens.emailObrigatorio);
     LoginPage.validarMensagemErro(mensagens.senhaObrigatoria);
+
+    cy.allure().endStep('passed');
   });
 
   it('Deve exibir erro ao tentar realizar login com email inválido', () => {
+    cy.allure().startStep('Início do teste: login com email inválido');
+
     const emailFake = faker.internet.email();
 
     LoginPage.preencherEmail(emailFake);
@@ -54,9 +66,13 @@ describe('Testes E2E - Login', () => {
 
     cy.url().should('include', '/login');
     LoginPage.validarMensagemErro(mensagens.emailOuSenhaInvalidos);
+
+    cy.allure().endStep('passed');
   });
 
   it('Deve exibir erro ao tentar realizar login com senha inválida', () => {
+    cy.allure().startStep('Início do teste: login com senha inválida');
+
     const senhaFake = faker.internet.password(8);
 
     LoginPage.preencherEmail(usuarios.validos.email);
@@ -65,5 +81,7 @@ describe('Testes E2E - Login', () => {
 
     cy.url().should('include', '/login');
     LoginPage.validarMensagemErro(mensagens.emailOuSenhaInvalidos);
+
+    cy.allure().endStep('passed');
   });
 });
